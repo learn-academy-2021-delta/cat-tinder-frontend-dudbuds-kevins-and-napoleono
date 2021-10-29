0f66a1a3612c32f8a549ff5c9c4ce7b7021ec329
LEARN Academy <learnacademy@LEARNs-Air.lan>
2021-10-29 09:36:33 -0700

    Library Removed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { NavLink } from 'react-router-dom'
 import dudLogo from '../assets/dud-logo.jpg'
-import { logDOM } from '@testing-library/dom'
 
 
 class Header extends Component {
